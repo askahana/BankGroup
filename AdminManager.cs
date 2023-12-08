8bfcf485bf074401d8a9a6b63065d7f30e2716ca
hanadasuka <hanadasuka@gmail.com>
2023-12-08 21:26:08 +0100

    a

---
+++ AdminManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank1209
+{
+    internal class AdminManager : IMeny
+    {
+        public void Meny(User loggedIn)
+        {
+            Console.WriteLine("Admin");
+
+            RegisterNewCustomer.AddNew();
+            ShowAllCustomer.ShowAll();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Bank1209
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ShowAllCustomer.ShowAll();
+            AccountManagementSystem.Assign();
+        }
+    }
+}
