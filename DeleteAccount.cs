2bf644bd79e5bf12708b5a655a81cb9df3bc835c
hanadasuka <hanadasuka@gmail.com>
2023-12-11 12:11:33 +0100

    Created 2 classes, OpenNewAccount and DeleteAccount.

---
+++ DeleteAccount.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank1209
+{
+    internal class DeleteAccount
+    {
+        // Är det account eller hela user?
+    }
+}
